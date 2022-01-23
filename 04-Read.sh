973ed4924e136002c45c8435f1881266e712e565
tejap <btejapramod@gmail.com>
2022-01-23 11:49:33 +0530

    Special Variable inputs

--- 04-Read.sh
+++ 04-Read.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#During the execution of read command need to give inputs during the execution, so it breaks the automation
 read -p "Enter your Name: " name
 echo "Your Name is $name"
 read -p "Enter your Course_Name: " course
