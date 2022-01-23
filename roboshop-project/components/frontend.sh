436f8a1fbc6d8d4dbbcc0ba488355168e42090f9
tejap <btejapramod@gmail.com>
2022-01-23 19:17:07 +0530

    Nginx service started

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-
-## yum install nginx -y
-## systemctl enable nginx
-## systemctl start nginx
-
-#Let's download the HTDOCS content and deploy under the Nginx path.
-
-## curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#
-#Deploy in Nginx Default Location.
-#
-## cd /usr/share/nginx/html
-## rm -rf *
-##
-## mv frontend-main/* .
-## mv static/* .
-## rm -rf frontend-master README.md
-## mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#
-#Finally restart the service once to effect the changes.
-
-## systemctl restart nginx
 #***********************************************************************
 #Delete the old content in the log file everytime we execute the script
 rm -f /tmp/roboshop.log
