a4de11e8b200f50b3e402a898c1daed72c0bd6c5
tejap <btejapramod@gmail.com>
2022-01-24 16:13:45 +0530

    EC2 instance creation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+LOG=/tmp/instnace-create.log
+rm -f $LOG
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
-  echo "Unable to find image AMI_ID"
+  echo -e "\e[1;31mUnable to find image AMI_ID\e[0m"
   else
-    echo "AMI_ID=${AMI_ID}"
+    echo -e "\e[1;33mAMI_ID=${AMI_ID}\e[0m"
 fi
