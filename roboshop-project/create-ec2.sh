e03186cc355ed0c50988c8971b559438440e2e5b
tejap <btejapramod@gmail.com>
2022-01-24 16:35:24 +0530

    EC2 instance creation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -2,8 +2,8 @@
 
 INSTANCE_NAME=$1
 if [ -z "$INSTANCE_NAME" ]; then
-echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
-exit
+   echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
+  exit
 fi
 LOG=/tmp/instnace-create.log
 rm -f $LOG
@@ -12,6 +12,9 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;31mUnable to find image AMI_ID\e[0m"
+  exit
   else
     echo -e "\e[1;33mAMI_ID=${AMI_ID}\e[0m"
 fi
+
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
