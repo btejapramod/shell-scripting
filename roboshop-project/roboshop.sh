d3f87eceaec164e83de8c1c0a5e0c0ace669c99b
tejap <btejapramod@gmail.com>
2022-01-24 18:35:23 +0530

    EC2 instance creation

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -4,5 +4,5 @@ if [ -f components/$1.sh ]; then
 bash components/$1.sh
 else
   echo -e  "\e[1;31mInvalid Input\e[0m"
-  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|user\e[0m"
+  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
   fi
