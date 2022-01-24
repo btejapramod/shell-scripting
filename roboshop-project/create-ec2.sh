d671a8779866890c19644a63694bce0f91404256
tejap <btejapramod@gmail.com>
2022-01-24 18:38:31 +0530

    EC2 instance creation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -4,6 +4,7 @@ bash components/$1.sh
 else
   echo -e  "\e[1;31mInvalid Input\e[0m"
   echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+  exit
   fi
 LOG=/tmp/instance-create.log
 rm -f $LOG
