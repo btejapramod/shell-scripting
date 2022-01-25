38e6816b30a5b0f1e2f9d8402074ca16d5a05b8e
tejap <btejapramod@gmail.com>
2022-01-25 12:59:15 +0530

    Adding changes

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,9 +1,11 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
-if [ $? -eq 0 ]; then
+STAT() {
+  if [ $1 -eq 0 ]; then
   echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILED\e[0m"
     exit
     fi
+    }
--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -4,64 +4,32 @@ source components/common.sh
 #Installing nginx package
 echo "Installing NGINX"
 yum install nginx -y &>>$LOG_FILE #redirecting to the content to /tmp/roboshop.log
-
-#Exit Status condition added
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
 
 #Download frontend code from repo
 echo "Downloading the frontend code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
+
 #Delete all directories and files in html directory
 echo "Delete the old content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
+
 #Extracting the frontend.zip file
 echo "Extracting the frontend code"
 unzip -o /tmp/frontend.zip &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
+
 echo "Copy the extracted content to Nginx path"
 cp -r frontend-main/static/* /usr/share/nginx/html &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
+
 echo "Copy the nginx roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
+
 echo "Start Nginx Service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILED\e[0m"
-    exit
-    fi
+STAT $?
