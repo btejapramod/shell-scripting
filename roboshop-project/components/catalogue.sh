2313a7b7c5f9f291e4e65c9ff4b6b6ee2b9f0a69
tejap <btejapramod@gmail.com>
2022-01-25 14:02:12 +0530

    Adding changes

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -1,86 +1,51 @@
 #!/bin/bash
-#1. This service is written in NodeJS, Hence need to install NodeJS in the system.
-#
-#```bash
-## curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
-## yum install nodejs gcc-c++ -y
-#
-#```
-#
-#1. Let's now set up the catalogue application.
-#
-#As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
-#
-#So to run the catalogue service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use `roboshop` as the username to run the service.
-#
-#```bash
-## useradd roboshop
-#
-#```
-#
-#1. So let's switch to the `roboshop` user and run the following commands.
-#
-#```bash
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
-#
-#```
-#
-#1. Update SystemD file with correct IP addresses
-#
-#    Update `MONGO_DNSNAME` with MongoDB Server IP
-#
-#2. Now, lets set up the service with systemctl.
-#
-#```bash
-## mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-## systemctl daemon-reload
-## systemctl start catalogue
-## systemctl enable catalogue
-#
-#```
 
 source components/common.sh
 
 echo "Setup NodeJS Repo"
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>LOG_FILE
+STAT $?
 
 echo "Install NodeJS"
 yum install nodejs gcc-c++ -y &>>LOG_FILE
+STAT $?
 
 echo "Create APP User"
 useradd roboshop &>>LOG_FILE
+STAT $?
 
 echo "Downloading the catalogue code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+STAT $?
 
 echo "Extract the Catalogue code"
 unzip /tmp/catalogue.zip &>>LOG_FILE
+STAT $?
 
 echo "Clean old content"
 rm -rf /home/roboshop/catalogue
+STAT $?
 
 echo "Copy catalogue content"
 cp -r catalogue-main catalogue &>>LOG_FILE
+STAT $?
 
 echo "Install the dependencies"
 cd /home/roboshop/catalogue
 npm install &>>LOG_FILE
+STAT $?
 
 chown roboshop:roboshop /home/roboshop/ -R
-
 echo "Update SystemD file"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
+STAT $?
 
 echo "Setup Catalogue SystemD file"
 mv /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service &>>$LOG_FILE
+STAT $?
 
 echo "Start Catalogue"
 systemctl daemon-relaod  &>>$LOG_FILE
 systemctl enable catalogue &>>$LOG_FILE
 systemctl start catalogue &>>$LOG_FILE
-
+STAT $?
--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -32,21 +32,27 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 
 echo "Install MongoDB"
 yum install -y mongodb-org &>>LOG_FILE
+STAT $?
 
 echo "Update the mogodb config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>LOG_FILE
+STAT $?
 
 echo "Start the Mongodb database"
 systemctl enable mongod &>>LOG_FILE
 systemctl start mongod &>>LOG_FILE
+STAT $?
 
 echo "Download the database schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>LOG_FILE
+STAT $?
 
 echo "Extract the schema"
 unzip -o /tmp/mongodb.zip &>>LOG_FILE
+STAT $?
 
 echo "Load the schema"
 cd mongodb-main
 mongo < catalogue.js &>>LOG_FILE
 mongo < users.js &>>LOG_FILE
+STAT $?
