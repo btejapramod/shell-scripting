c19e2b715fcdf691fd3a1ec8207539a5c2bec6b1
tejap <btejapramod@gmail.com>
2022-01-25 14:38:31 +0530

    Adding changes

--- roboshop-project/components/redis.sh
+++ roboshop-project/components/redis.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#
-#1. Install Redis.
-#
-#```bash
-#
-## curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-## yum install redis -y
-#```
-#
-#2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
-#
-#3. Start Redis Database
-#
-#```bash
-## systemctl enable redis
-## systemctl start redis
-#```
 source components/common.sh
 
 echo "Configuring the repos"
@@ -30,7 +13,7 @@ echo "Update Redis Config file"
 if [ -f /etc/redis.conf ]; then
 sed -i -e "s/127.0.0.0/0.0.0.0/g" /etc/redis.conf &>>$LOG_FILE
 elif [ -f /etc/yum.repos.d/redis/repo ]; then
-  sed -e "s/127.0.0.0/0.0.0.0/g" etc/yum.repos.d/redis.repo &>>$LOG_FILE
+  sed -e "s/127.0.0.0/0.0.0.0/g" e/etc/redis.conf &>>$LOG_FILE
   fi
 STAT $?
 
--- roboshop-project/components/user.sh
+++ roboshop-project/components/user.sh
@@ -1,2 +1,44 @@
+#!/bin/bash
 
-echo Installing user
+1. This service is written in NodeJS, Hence need to install NodeJS in the system.
+
+```bash
+# curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
+# yum install nodejs gcc-c++ -y
+```
+
+1. Let's now set up the User application.
+
+As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
+
+So to run the User service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use `roboshop` as the username to run the service.
+
+```bash
+# useradd roboshop
+```
+
+1. So let's switch to the `roboshop` user and run the following commands.
+
+```bash
+$ curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+$ cd /home/roboshop
+$ unzip /tmp/user.zip
+$ mv user-main user
+$ cd /home/roboshop/user
+$ npm install
+```
+
+1. Update SystemD service file,
+
+    Update `REDIS_ENDPOINT` with Redis Server IP
+
+    Update `MONGO_ENDPOINT` with MongoDB Server IP
+
+2. Now, lets set up the service with systemctl.
+
+```bash
+# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+# systemctl daemon-reload
+# systemctl start user
+# systemctl enable user
+```
