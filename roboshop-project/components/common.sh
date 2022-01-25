a9da42a829e7fe94be2dd553403be4dd9b1ef253
tejap <btejapramod@gmail.com>
2022-01-25 15:42:30 +0530

    Adding changes

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -9,3 +9,53 @@ STAT() {
     exit 2
     fi
     }
+NODEJS(){
+COMPONENT=$1
+echo "Setup NodeJS Repo"
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>LOG_FILE
+STAT $?
+
+echo "Install NodeJS"
+yum install nodejs gcc-c++ -y &>>LOG_FILE
+STAT $?
+
+echo "Create APP User"
+useradd roboshop &>>LOG_FILE
+STAT $?
+
+echo "Downloading the User code"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>LOG_FILE
+STAT $?
+
+echo "Extract the $COMPONENT code"
+unzip /tmp/$COMPONENT.zip &>>LOG_FILE
+STAT $?
+
+echo "Clean old content"
+rm -rf /home/roboshop/$COMPONENT
+STAT $?
+
+echo "Copy $COMPONENT content"
+cp -r $COMPONENT-main $COMPONENT &>>LOG_FILE
+STAT $?
+
+echo "Install the dependencies"
+cd /home/roboshop/$COMPONENT
+npm install &>>LOG_FILE
+STAT $?
+
+chown roboshop:roboshop /home/roboshop/ -R
+echo "Update SystemD file"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$COMPONENT/systemd.service &>>$LOG_FILE
+STAT $?
+
+echo "Setup $COMPONENT SystemD file"
+mv /home/roboshop/$COMPONENT/systemd.service  /etc/systemd/system/$COMPONENT.service &>>$LOG_FILE
+STAT $?
+
+echo "Start $COMPONENT"
+systemctl daemon-relaod  &>>$LOG_FILE
+systemctl enable $COMPONENT &>>$LOG_FILE
+systemctl start $COMPONENT &>>$LOG_FILE
+STAT $?
+}
