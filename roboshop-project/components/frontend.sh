da64aabc0a5ace83678e4308613c57e2a3e9e907
tejap <btejapramod@gmail.com>
2022-01-26 12:32:50 +0530

    Adding changes

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -30,7 +30,7 @@ cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 STAT $?
 
 echo "Update RoboShop Config"
-sed -i -e "/catalogue/ s/localhost/catalogue.roboshop.internal/" -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e "/catalogue/ s/localhost/catalogue.roboshop.internal/" -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STAT $?
 
 echo "Start Nginx Service"
