475b3fb3d5d1ad49646033d3adb0c61a280b8c4d
tejap <btejapramod@gmail.com>
2022-01-27 14:32:45 +0530

    Adding changes

--- roboshop-project/components/payment.sh
+++ roboshop-project/components/payment.sh
@@ -40,9 +40,9 @@ source components/common.sh
 #
 #3. Setup the service
 #
-#```sql
+
 ## mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
 ## systemctl daemon-reload
 ## systemctl enable payment
 ## systemctl start payment
-#```
+
