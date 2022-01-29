6dd58fa825a59f0d6c73438f210528f8cb10cdce
tejap <btejapramod@gmail.com>
2022-01-29 10:47:59 +0530

    Adding changes

---
+++ currency.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl https://www.google.com/search?q=dollar+to+inr&oq=doll&aqs=chrome.2.0i433i512j69i57j0i433i512j0i131i433i512l3j0i512j0i433i512j0i131i433i512j0i433i512.10838j0j7&sourceid=chrome&ie=UTF-8
