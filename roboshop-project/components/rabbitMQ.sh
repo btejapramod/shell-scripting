3c7bf49d74bc59c90dd0a9cd11622690871296aa
tejap <btejapramod@gmail.com>
2022-01-29 11:40:05 +0530

    Adding changes

--- roboshop-project/components/rabbitMQ.sh
+++ roboshop-project/components/rabbitMQ.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 source components/common.sh
 
-echo "Setup the RabbitMQ Repo and Erlang"
+echo "Configure YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG_FILE
+STAT $?
+
+echo "Install Rabbitmq and Erlang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
 STAT $?
 
 echo "Install the RabbitMQ Server"
